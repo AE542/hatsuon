68b2f1123fe310cf795bc56259219c034eb2cb87
Mohammed Qureshi <mqureshi93v2@gmail.com>
2023-06-19 18:41:52 +0100

    ContentView changed and layout started

--- Hatsuon App V.1/ContentView.swift
+++ Hatsuon App V.1/ContentView.swift
@@ -4,12 +4,14 @@
 //
 //  Created by Mohammed Qureshi on 2021/07/11.
 //
+//2020/07/11 First Prototype using SwiftUI to build this. Check notes for the ideas to get it working. Might need to add ImageAPI to get pictures to show up.
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Hatsuon!")
+            .font(.title)
             .padding()
     }
 }
